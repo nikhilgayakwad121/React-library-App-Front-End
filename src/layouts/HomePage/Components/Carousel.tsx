6be8fdf3a8b7ef1d65fae002d2bbdb7286cc82d0
Nikhil Gayakwad <nikhilgayakwad121@gmail.com>
2024-09-11 18:12:19 +0530

    Added the error handling for the rest API's

--- src/layouts/HomePage/Components/Carousel.tsx
+++ src/layouts/HomePage/Components/Carousel.tsx
@@ -12,13 +12,65 @@ export const Carousel = () => {
     useEffect(() => {
         const fetchBooks = async () => {
 
+            const baseUrl : string = "http://localhost:8080/api/books";
+
+            const url: string = `${baseUrl}?page=0&size=9`;
+
+            const response = await  fetch(url);
+
+            if(!response.ok){
+                throw new Error('Something went wrong!');
+
+            }
+
+            const responseJson = await response.json();
+
+            const responseData = responseJson._embedded.books;
+
+            const loadedBooks : BookModel[] = [];
+
+            for(const key in responseData) {
+                loadedBooks.push({
+                    id: responseData[key].id,
+                    title: responseData[key].title,
+                    author: responseData[key].author,
+                    description: responseData[key].description,
+                    copies: responseData[key].copies,
+                    copiesAvailable: responseData[key].copiesAvailable,
+                    category: responseData[key].category,
+                    img: responseData[key].img,
+                });
+            }
+
+            setBooks(loadedBooks);
+            setIsLoading(false);
+
         };
+
+
         fetchBooks().catch((error:any) => {
             setIsLoading(false);
             setHttpError(error.message);
         })
 
     }, []);
+
+    if(isloading){
+        return(
+            <div className="contaner m-5">
+                <p>Loading...</p>
+            </div>
+        )
+    }
+
+    if(httpError){
+        return(
+            <div className="container m-5">
+                <p>{httpError}</p>
+            </div>
+
+        )
+    }
 
     return (
         <div className='container mt-5' style={{ height: 550 }}>
