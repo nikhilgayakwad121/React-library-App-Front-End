c4d5cfe031861cc92d5985f2f34c64c6ae23b49f
Nikhil Gayakwad <nikhilgayakwad121@gmail.com>
2024-09-16 17:29:42 +0530

    Last commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe('pk_test_51PUulgRsYiIzrGNy2OcHa2JrqaHUMXOwlUpRcLIrPSobReHqhJCqMrx5un3BZaGvxXsTluObpDMeMiWsZOsOykGV00zYjQvVhp');
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <App />
+    <Elements stripe={stripePromise}>
+      <App />
+    </Elements>
   </BrowserRouter>
 );
